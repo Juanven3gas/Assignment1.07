e9ebb70fcc9e342e9d6bd3dc584530b1994d44d3
Juan Venegas <juanven3gas@gmail.com>
2017-10-31 10:48:15 -0500

    Added some functionality but break when there is just a newline character

--- main.cpp
+++ main.cpp
@@ -14,10 +14,10 @@ int main(int argc, char* argv[])
     * get the dir
     */
     char *home = getenv("HOME");
-    int length = strlen(home) + strlen("/.rlg327/maonster.txt") +1;
+    int length = strlen(home) + strlen("/.rlg327/maonster_desc.txt") +1;
     char *path = (char*)malloc(length);
     strcpy(path, home);
-    strcat(path, "/.rlg327/monster.txt");
+    strcat(path, "/.rlg327/monster_desc.txt");
 
     /**
     * Begin attepmting to open file if not terminate
@@ -34,12 +34,78 @@ int main(int argc, char* argv[])
         
         char buffer[256];
         int startFound = 0;
+        
+
+        //If the first line of the file does not match the header then exit program
+        monsterFile.getline(buffer, 256);
+
+        if(strcmp(buffer, "RLG327 MONSTER DESCRIPTION 1") != 0)
+        {
+            cout << "Incorrect file header\n Exiting program" << endl;
+            free(path);
+            monsterFile.close();
+            return -1;
+        }
+        else
+        {
+            cout << "File has correct header" << endl;
+            monsterFile.getline(buffer, 256);
+        }
 
         while(!monsterFile.eof())
         {
             monsterFile.getline(buffer, 256);
-            cout << buffer << endl;
+            const char s[2] = " ";
+            char *token;
+            token = strtok(buffer,s);
+            
+            while(1)
+            {
+                if(strcmp (token, "NAME") == 0)
+                {
+                    while(token != NULL)
+                    {
+                        token = strtok(NULL, s);
+                        printf("%s ", token);
+                    }
+                    printf("\n");
+                    break;
+                }
+                else if(strcmp(token, "SYMB") == 0)
+                {
+                    while(token != NULL)
+                    {
+                        token = strtok(NULL, s);
+                        printf("%s ", token);
+        
+                    }
+                    printf("\n");
+                    break;
+                }
+                else if(strcmp(token, "COLOR") == 0)
+                {
+                    while (token != NULL)
+                    {
+                        token = strtok(NULL, s);
+                        printf("%s ", token);
+
+                    }
+                    printf("\n");
+                    break;
+                }
+                else
+                {
+                    cout << "reached else statement" << endl;
+                    token = NULL;
+                    break;
+                }
+                //printf("%s\n", token);  
+            }
+            //cout << buffer << endl;
         }
+    }else
+    {
+        cout << "File could not be found or open" << endl;
     }
     
     /**
